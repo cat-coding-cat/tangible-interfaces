dad2b458756d0d22e17d4802b5bee50eb61ffaad
cat-coding-cat <70901395+cat-coding-cat@users.noreply.github.com>
2022-03-04 09:47:10 +0100

    Update fexample2.ino

--- fexample2/fexample2.ino
+++ fexample2/fexample2.ino
@@ -1,1 +1,36 @@
-void
+int numData;
+  float data[16];
+  int j;
+  float average;
+  float bucket;
+  
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Number of data ?");
+  while (Serial.available()==0){
+    
+    }
+    numData = Serial.parseInt();
+    for (j=1; j<numData;j=j+1){
+      Serial.println("input: ");
+      while (Serial.available()==0){
+    
+    }
+     data[j]=Serial.parseFloat(); 
+      }
+      for(j=1;j<=numData;j=j+1){
+        bucket = bucket + data[j];
+        }
+        average = bucket/numData;
+        bucket = 0;
+        Serial.println("The data are: ");
+              for(j=1;j<=numData;j=j+1){
+                Serial.println(data[j]);
+              }
+              Serial.print("");
+              Serial.print("average  : ");
+              Serial.println(average);
+}
